e30ee00ab656e801ec92e4cc30cce67c9a5a282b
Samarth Jugran <jugransamarth@gmail.com>
2020-10-05 11:15:01 +0530

    add fedora/grub script

--- fedora-switch.sh
+++ fedora-switch.sh
@@ -24,6 +24,10 @@ else
 		echo "Switching to nvidia graphics..."
 		grubby --update-kernel=ALL --args="rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nvidia-drm.modeset=1"
 		grubby --update-kernel=ALL --remove-args="rd.driver.blacklist=nvidia,nvidia_uvm,nvidia_drm,nvidia_modeset modprobe.blacklist=nvidia,nvidia_uvm,nvidia_drm,nvidia_modeset nouveau.modeset=0"
+		
+		DIR="$(dirname "$(readlink -f "$0")")"
+		
+		cd "$DIR"
 
 		cp -p nvidia.conf /etc/X11/xorg.conf.d/nvidia.conf
 	
